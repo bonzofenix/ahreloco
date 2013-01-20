e91d663ce44986e5d9774e41ee2843aeebb041ff
bonzofenix <bonzofenix@gmail.com>
2013-01-20 17:48:00 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahreloco::Application.config.secret_token = 'e8301695fa422c2f69c1836fce5b95592f0edf37313af814e2af6d5ce1dc18ff97204d59a67263257a1d4cd28eb88efff7827e1784866731a681e8687d9ef0a5'
