e01f220ac7e725d7ec9214b6745082aad9b9ebd8
bonzofenix <bonzofenix@gmail.com>
2013-02-03 16:48:46 -0300

    add video wip

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.full_host = "http://localhost:8080"
+Ahreloco::Application.config.middleware.use OmniAuth::Builder do
+  provider :youtube, ENV['YOUTUBE_KEY'], ENV['YOUTUBE_SECRET'],
+    {:access_type => 'online', :approval_prompt => ''}
+end
