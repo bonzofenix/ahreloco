7945f37f9b490ebd9b08792b52e9033d279b9bce
bonzofenix <bonzofenix@gmail.com>
2013-02-27 20:59:30 -0300

    omniauth return call

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
-if Rails.env.production?
+#  OmniAuth.config.full_host = "http://localhost:8080"
   OmniAuth.config.full_host = 'http://ahreloco.herokuapp.com'
-else
-  OmniAuth.config.full_host = "http://localhost:8080"
-end
 
 Ahreloco::Application.config.middleware.use OmniAuth::Builder do
   provider :youtube, ENV['YOUTUBE_KEY'], ENV['YOUTUBE_SECRET'],
