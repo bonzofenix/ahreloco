97c9e25e2590555cdb3b8885e58293a7808537a0
bonzofenix <bonzofenix@gmail.com>
2013-02-27 20:34:40 -0300

    fullhost name removed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.full_host = "http://localhost:8080"
+#OmniAuth.config.full_host = "http://localhost:8080"
 Ahreloco::Application.config.middleware.use OmniAuth::Builder do
   provider :youtube, ENV['YOUTUBE_KEY'], ENV['YOUTUBE_SECRET'],
     {:access_type => 'online', :approval_prompt => ''}
