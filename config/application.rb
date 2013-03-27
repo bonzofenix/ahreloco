d69896bf01c0649de18570a546a525d6faf4e430
bonzofenix <bonzofenix@gmail.com>
2013-03-27 02:24:14 -0300

    test translations aa

--- config/application.rb
+++ config/application.rb
@@ -51,10 +51,9 @@ module Ahreloco
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     config.i18n.default_locale = :es
     config.i18n.locale = :es
-    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     I18n.locale = :es
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
