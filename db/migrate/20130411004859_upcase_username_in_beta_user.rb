b282f58b2121a18945ae52c63fd2235b3b2c270e
bonzofenix <bonzofenix@gmail.com>
2013-04-10 22:18:56 -0300

    beta user lowcase usename

--- db/migrate/20130411004859_upcase_username_in_beta_user.rb
+++ db/migrate/20130411004859_upcase_username_in_beta_user.rb
@@ -1,6 +1,6 @@
 class UpcaseUsernameInBetaUser < ActiveRecord::Migration
   def up
-    execute "UPDATE beta_users SET email = LOWER(username)"
+    execute "UPDATE beta_users SET username= LOWER(username)"
   end
 
   def down
