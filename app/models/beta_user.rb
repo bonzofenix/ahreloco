0e0badbd7f3812cd75d0a1bae6763813b3ab652b
bonzofenix <bonzofenix@gmail.com>
2013-04-26 22:40:12 -0300

    logging for beta signin

--- app/models/beta_user.rb
+++ app/models/beta_user.rb
@@ -3,7 +3,7 @@ class BetaUser < ActiveRecord::Base
   attr_accessible :active, :email, :username
   def self.can_login?(username)
     q = BetaUser.where('username LIKE ? AND active = ?', "%#{username.downcase}%", true)
-    puts "beta sessions: #{q.sql}"
+    puts "beta sessions: #{q.to_sql}"
     !q.all.empty? 
   end
 end
